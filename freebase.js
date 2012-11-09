59e164366b655861c031e8c4aec37d7334d27664
spencer kelly <spencerwater@gmail.com>
2012-11-09 14:19:49 +0000

    Default callback

--- freebase.js
+++ freebase.js
@@ -1,43 +1,75 @@
 var API_KEY='';//this is needed for heavy stuff
-
+var async_max=2;//the hardest we will concurrently hit freebase
+var host='https://www.googleapis.com/freebase/v1/';
 
 var request = require('request');
 var singularize=require('./singularize').singularize;
 var sentence=require('./sentence').sentenceparser;
-
+var async = require('async');
+var _=require('underscore');
 
 //main methods to freebase apis
 
 //interface to freebase's mql api
 exports.mqlread=function(query, options, callback){
+  callback=callback||console.log;
   if(!query){return callback({})}
-  options=options||{}
-  options.uniqueness_failure=options.uniqueness_failure||"soft"
-  options.cursor=options.cursor||""
+  options=options||{};
+  options.uniqueness_failure=options.uniqueness_failure||"soft";
+  options.cursor=options.cursor||"";
+    //is it an array of sub-queries?
+  if(_.isArray(query) && query.length>1){
+    return doit_async(query, exports.mqlread, options, callback)
+  }
   var params=set_params(options)
-  var url='https://www.googleapis.com/freebase/v1/mqlread?query='+encodeURIComponent(JSON.stringify(query))+'&'+params;
+  var url= host+'mqlread?query='+encodeURIComponent(JSON.stringify(query))+'&'+params;
   http(url, function(result){
     callback(result)
   })
 }
 
 //topic api
-exports.topic=function(q, filter, callback){
+exports.topic=function(q, options, callback){
+    callback=callback||console.log;
     if(!q){return callback({})}
-    get_id(q, {}, function(id){
-      var url='https://www.googleapis.com/freebase/v1/topic'+id;
-      if(filter){url+='?filter='+encodeURIComponent(filter)}
+    options=options||{};
+     //is it an array of sub-tasks?
+    if(_.isArray(q) && q.length>1){
+      return doit_async(q, exports.topic, options, callback)
+    }
+    get_id(q, options, function(id){
+      if(!id){return callback({})}
+      var url= host+'topic'+id;
+      if(options.filter){url+='?filter='+encodeURIComponent(options.filter)}
       http(url, function(result){
         callback(result)
       })
     })
 }
+//exports.topic("marvin gaye",{},console.log)
+
+//regular search api
+exports.search=function(q, options, callback){
+      callback=callback||console.log;
+      if(!q){return callback({})}
+      options=options||{};
+      options.query=q;
+      var params=set_params(options)
+      var url= host+'search/?'+params;
+      http(url, function(result){
+        if(!result || !result.result || !result.result[0] ){return callback([])}
+        callback(result.result)
+    })
+}
+
 
 //turn a string into a confident topic id
 exports.lookup=function(q, options, callback){
-  options=options||{}
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
   options.type=options.type||"/common/topic";
-  var url='https://www.googleapis.com/freebase/v1/search?limit=2&lang=en&type='+options.type+'&filter='
+  var url= host+'search?limit=2&lang=en&type='+options.type+'&filter='
   var filter=encodeURIComponent('(any name{full}:"'+q+'" alias{full}:"'+q+'")')
   http(url+filter, function(result){
     if(!result || !result.result || !result.result[0] ){return callback([])}
@@ -65,19 +97,18 @@ exports.lookup=function(q, options, callback){
   })
 }
 
-
 //get all of the results to your query
 exports.paginate=function(query, options, callback){
+  callback=callback||console.log;
   if(!query){return callback({})}
-  options=options||{}
-  query[0].limit=query[0].limit||170//force a safe but efficient lookup
+  options=options||{};
+  //query[0].limit=query[0].limit||170  //force a safe but efficient lookup
   var data=[];
   //recursive mqlread until cursor is false
   iterate('')
   function iterate(cursor){
     options.cursor=cursor
     exports.mqlread(query, options, function(result){
-      console.log(data.length)
       data=data.concat(result.result)
       if(result.cursor && (!options.max || data.length<options.max) ){
         iterate(result.cursor)
@@ -94,22 +125,27 @@ exports.paginate=function(query, options, callback){
 
 
 //turns a url into a freebase topic and list its same:as links
-exports.same_as_links=function(q, callback){
+exports.same_as_links=function(q, options, callback){
+  callback=callback||console.log;
   if(!q){return callback({})}
-  var url='https://www.googleapis.com/freebase/v1/search?type=/common/topic&limit=1&query='+encodeURIComponent(q)
+  options=options||{};
+  options.filter=options.filter||"/common/topic"
+
+  var url= host+'search?type=/common/topic&limit=1&query='+encodeURIComponent(q)
   http(url, function(result){
     if(!result || !result.result || !result.result[0]){
       return callback({})
     }
       //get its formatted links from the topic api
-    exports.topic(result.result[0].mid , "/common/topic", function(data){
+    exports.topic(result.result[0].mid , options, function(data){
       var links=[];
-      //same-asy ones
+      //same-as ones
       if(data.property['/common/topic/topic_equivalent_webpage']){
        links=data.property['/common/topic/topic_equivalent_webpage'].values.map(function(v){
           return {href:v.value, title:parseurl(v.value).authority}
         })
       }
+      //webpage ones
       if(data.property['/common/topic/topical_webpage']){
        links=links.concat(data.property['/common/topic/topical_webpage'].values.map(function(v){
           var host=parseurl(v.value).authority || ''
@@ -122,19 +158,22 @@ exports.same_as_links=function(q, callback){
   })
 }
 
-
-
-
 //return specific language title for a topic
-exports.translate=function(q, lang, callback){
-  if(!lang || !q){return callback('')}
-    lang='/lang/'+lang;
-    get_id(q, {}, function(id){
+exports.translate=function(q, options, callback){
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+
+  if(!options.lang){options.lang="/lang/fr"}//defaulting to french is better than an error..?
+  if(!options.lang.match(/\/lang\//)){
+    options.lang='/lang/'+options.lang
+  }
+    get_id(q, options, function(id){
     if(!id){return callback("")}
     var query=[{
       "id": id,
       "name": [{
-        "lang":  lang,
+        "lang":  options.lang,
         "value": null
       }]
     }]
@@ -149,17 +188,38 @@ exports.translate=function(q, lang, callback){
 
 
 
-//
-exports.wikipedia_link=function(query, options, callback){
+//get a url for wikipedia based on this topic
+exports.wikipedia_link=function(q, options, callback){
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
 
+   get_id(q, options, function(id){
+     if(!id){return callback("")}
+     var query=[{
+        "id":   id,
+        "name": null,
+        "key": {
+          "namespace": "/wikipedia/en_title",
+          "value":     null
+        }
+      }]
+    exports.mqlread(query, options, function(result){
+      if(!result || !result.result || !result.result[0]){return callback('')}
+      return callback('http://en.wikipedia.org/wiki/'+result.result[0].key.value)
+    })
+  })
 }
 
 //get a text blurb from freebase
 exports.description=function(q, options, callback){
- if(!q){return callback("")}
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+
  get_id(q, options, function(id){
   if(!id){return callback("")}
-  var url='https://www.googleapis.com/freebase/v1/text/'+id;
+  var url= host+'text/'+id;
   http(url,function(result){
     if(!result.result){return callback('')}
     callback(result.result)
@@ -167,10 +227,25 @@ exports.description=function(q, options, callback){
  });
 }
 
+//get a topics notable type
+exports.notable=function(q, options, callback){
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+
+ exports.topic(q, {filter:"/common/topic/notable_for"}, function(result){
+  if(!result || !result.property || !result.property['/common/topic/notable_for']){return callback({})}
+  var notable=result.property['/common/topic/notable_for'] || {values:[]};
+  return callback(notable.values[0])
+ });
+}
 
 //get the first sentence of a topic description
 exports.sentence=function(q, options, callback){
-  if(!q){return callback("")}
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+
   exports.description(q, options, function(desc){
     if(!desc){return callback("")}
     desc=sentence(desc)||[]
@@ -183,9 +258,11 @@ exports.sentence=function(q, options, callback){
 
 //get a list of topics in a type
 exports.list=function(q, options, callback){
-  if(!q){return callback([])}
-    options=options||{}
-    options.max=options.max || 500;
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+  options.max=options.max || 500;
+
   //singularize it if it's a search query
   if(!q.match(/\/.{1,12}\/.{3}/)){
     q=singularize(q);
@@ -196,12 +273,44 @@ exports.list=function(q, options, callback){
     query=[{"type":id,"name":null, "id":null}]
     exports.paginate(query, options, callback)
    })
+}
 
+
+//get any incoming data to this topic, //ignoring cvt types
+exports.incoming=function(q, options, callback){
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+
+  get_id(q, options, function(id){
+    if(!id){return callback([])}
+    var query=[{
+      "id": id,
+      "/type/reflect/any_reverse": [{
+        "link": null,
+        "id":   null,
+        "name": null,
+        "type": "/common/topic",
+        "limit":170
+      }]
+    }]
+    //this technically doesn't paginate.
+    exports.paginate(query, options, function(result){
+      if(!result || !result[0] || !result[0]['/type/reflect/any_reverse']){
+        return callback([])
+      }
+      return callback(result[0]['/type/reflect/any_reverse'])
+    })
+  })
 }
 
 //return all outgoing links for a topic, traversing cvt types
-exports.graph=function(q, options, callback){
-  exports.topic(q, '', function(result){
+exports.outgoing=function(q, options, callback){
+  callback=callback||console.log;
+  if(!q){return callback({})}
+  options=options||{};
+
+  exports.topic(q, {}, function(result){
     var out=[];
       //get rid of permissions and stuff..
     result.property=kill_boring(result.property)
@@ -244,7 +353,7 @@ exports.graph=function(q, options, callback){
 
 //slightly different lookup when its a url
 function url_lookup(q, callback){
-  var url='https://www.googleapis.com/freebase/v1/search?type=/common/topic&limit=1&query='+encodeURIComponent(q)
+  var url= host+'search?type=/common/topic&limit=1&query='+encodeURIComponent(q)
   http(url, function(result){
     callback(result)
   })
@@ -276,7 +385,7 @@ function get_id(q, options, callback){
 }
 
 
-//properties that don't feel graphy
+//kill the freebase internal-properties that don't feel graphy
 function kill_boring(obj){
   var boring=[
     "/type/object/attribution",
@@ -335,16 +444,15 @@ function parseurl (str) {
 };
 
 function http(url, callback){
+   if(!url){return callback({error:"bad url"})}
    request({
         uri: url
     }, function(error, response, body) {
         try{
           body=JSON.parse(body)
-        }catch(e){console.log(e)}
+        }catch(e){return callback({error:e})}
         if (error || response.statusCode != 200 ) {
-              console.log('----error----');
-              console.log(body);
-              return callback([])
+              return callback({error:error})
               }
         return callback(body);
     })
@@ -356,35 +464,36 @@ function set_params(options){
   return Object.keys(options).map(function(v){return v+'='+options[v]}).join('&')
 }
 
-//get_id("thom yorke", {}, console.log)   // /m/01p0w_
-//get_id('http://toronto.ca', {}, console.log)
-
-// exports.lookup("tom green", {}, console.log})  ///m/017yxq
-
-// exports.same_as_links("toronto", console.log)
-// exports.same_as_links("http://www.geonames.org/6167865/", console.log)
-// exports.same_as_links("/m/0h7h6", console.log)
-
-
-// exports.lookup("http://myspace.com/u2", console.log})
-
-
-// query=[{ "type": "/music/album", "id": "/en/keep_it_turned_on", "artist" : null}]
-// exports.mqlread(query,{},console.log)
-
-//exports.graph("ubuntu",'',console.log)
-
-// query=[{"type":"/base/disaster2/tornado","name":null}]
-// exports.paginate(query, {max:100}, console.log)
+//turn an array into smaller groups of arrays
+  function groups_of(arr, group_length){
+    var all=[]
+    for(var i in arr){
+      if(i%group_length==0){
+        all.push([arr[i]])
+      }else{
+        all[all.length-1].push(arr[i])
+      }
+    }
+    return all
+    }
 
 
-//exports.list("earthquakes", {}, console.log)
+//handle rate-limited asynchronous freebase calls with a ending callback
+function doit_async(arr, fn, options, done){
+    //wrap them all in functions
+  var function_list=arr.map(function(r){
+      return function(callback){
+              fn(r, options, function(r){
+                callback(null, r);
+              })
+      }
+    })
+  //groups of async tasks in a synchonous task
+  var all=groups_of(function_list, async_max).map(function(f_group){
+    return function(callback){async.parallel(f_group, callback  );}
+  })
+  async.series(all, function(err, result){
+    done(_.flatten(result))
+  });
+}
 
-
-// exports.description("mike myers", {}, console.log)
-// exports.description("http://myspace.com/u2", {}, console.log)
-
-//exports.sentence("thom yorke",{},console.log)
-
-//exports.translate("toronto","zh",console.log)//多伦多
-//exports.translate("radiohead","ko",console.log)//라디오헤드
