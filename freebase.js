a0ca08d847e739b19a7ee7c3bfa92a6ebd104e02
Spencer Kelly <spencermountain@gmail.com>
2013-05-30 19:11:42 +0100

    write loop

--- freebase.js
+++ freebase.js
@@ -1,4 +1,3 @@
-
 if (typeof module !== 'undefined' && module.exports) {
   var request = require("request");
   var fns = require('./helpers');
@@ -14,9 +13,9 @@ var freebase = (function() {
   freebase.api_key = "AIzaSyD5GmnQC7oW9GJIWPGsJUojspMMuPusAxI" //please don't abuse my key
   freebase.host = "https://www.googleapis.com/freebase/v1" //production
   //freebase.host = "https://www.googleapis.com/freebase/v1sandbox"  //sandbox
-  freebase.data=data;
-  freebase.wikipedia=wikipedia
-  freebase.default_callback = console.log
+  freebase.data = data;
+  freebase.wikipedia = wikipedia
+  freebase.default_callback = function(r){console.log(JSON.stringify(r, null, 2));}
 
   //
   //Freebase classes
@@ -42,39 +41,65 @@ var freebase = (function() {
     topic.mid = data.mid || data.id;
     topic.name = data.name || data.text || data.title;
     topic.get = function(property, callback, options) {
-      options=new freebase.Option(options)
-      callback=callback||freebase.default_callback;
-      freebase.reconcile_property(property, function(p){
-        options.property=p.id;
-        freebase.topic_api(topic, function(result){
-          if(!result.property || result.property.length==0){
+      options = new freebase.Option(options)
+      callback = callback || freebase.default_callback;
+      freebase.reconcile_property(property, function(p) {
+        options.property = p.id;
+        freebase.topic_api(topic, function(result) {
+          if (!result.property || result.property.length == 0) {
             callback([])
-          }else{
+          } else {
             return callback(render_results(result.property[options.property]))
           }
-        },options)
-      },options)
+        }, options)
+      }, options)
     }
-    topic=fns.extend(topic, data)
-  }
+    topic.set = function(property, id, callback, options) {
+      options = new freebase.Option(options)
+      callback = callback || freebase.default_callback;
+      freebase.reconcile_property(property, function(prop) {
+         var query = {
+          id: topic.id,
+        }
+        query[prop.id]={
+          id:id,
+          connect: (options.connect || "insert")
+        }
+        freebase.mqlwrite(query, function(result) {
+          if(!result || !result.result || !result.result.id){
+            console.log("=======error=====")
+            console.log(JSON.stringify(result, null, 2));
+            console.log("=======error=====")
+          }else{
+            console.log([result.result.id, result.result[prop.id].id, result.result[prop.id].connect, result.result.name].join("   \t"))
+          }
+          callback(result)
+        }, options)
+      })
+    }
+    topic.add_type = function(type, callback, options) {
+      topic.set("/type/object/type", type, callback, options)
+    }
+  topic = fns.extend(topic, data)
+}
 
-function render_results(results){
-  results=results||{}
-  if(results.valuetype=="string"|| results.valuetype=="uri"|| results.valuetype=="int"){
-    return results.values.map(function(v){
+function render_results(results) {
+  results = results || {}
+  if (results.valuetype == "string" || results.valuetype == "uri" || results.valuetype == "int") {
+    return results.values.map(function(v) {
       return v.text
     })
   }
-  if(results.valuetype=="datetime"){
-    return results.values.map(function(v){
+  if (results.valuetype == "datetime") {
+    return results.values.map(function(v) {
       return fns.parsedate(v.text)
     })
   }
-  if(results.valuetype=="object"){
-    return results.values.map(function(v){
+  if (results.valuetype == "object") {
+    return results.values.map(function(v) {
       return {
-        id:v.id,
-        name:v.text
+        id: v.id,
+        name: v.text
       }
     })
   }
@@ -82,152 +107,198 @@ function render_results(results){
 }
 
 
-  freebase.Property = function(data) {
-    var property = this;
-    var type = fns.jstype(data);
-    if (type == "null") {
-      return {}
-    }
-    if (type == "string") {
-      if (fns.isid(data)) {
-        data = {
-          id: data
-        }
-      } else {
-        //search automatically?
-        console.log(JSON.stringify(data) + " is not a freebase id")
-        process.exit(code = 1)
-      }
-    }
-    property.id = data.id || data.mid;
-    property.mid = data.mid || data.id;
-    property.type = (function() {
-      return property.mid.split(/\//g).slice(0, 3).join('/')
-    })()
-    //property=fns.extend(property, data)
+freebase.Property = function(data) {
+  var property = this;
+  var type = fns.jstype(data);
+  if (type == "null") {
+    return {}
   }
-
-  freebase.Topiclist = function(data) {
-    var list = this;
-    var type = fns.jstype(data);
-    if (type == "null") {
-      return {}
-    }
-    list.data = data.map(function(v) {
-        return new freebase.Topic(v)
-      })
-    list.get = function(property, callback, options){
-      var doit=function(t, cb){
-        t.get(property, cb, options)
+  if (type == "string") {
+    if (fns.isid(data)) {
+      data = {
+        id: data
       }
-      slow(list.data, doit, {}, function(r){
-        list.data=list.data.map(function(l,i){
-          l[property]=r[i]
-          return l
-        })
-        callback(list.data)
-      })
+    } else {
+      //search automatically?
+      console.log(JSON.stringify(data) + " is not a freebase id")
+      process.exit(code = 1)
     }
-    list.set=function(){}
-    list.select=function(){}
-    list.reject=function(){}
+  }
+  property.id = data.id || data.mid;
+  property.mid = data.mid || data.id;
+  property.type = (function() {
+    return property.mid.split(/\//g).slice(0, 3).join('/')
+  })()
+  //property=fns.extend(property, data)
+}
+
+freebase.Topiclist = function(data) {
+  var list = this;
+  var type = fns.jstype(data);
+  if (type == "null") {
+    return {}
+  }
+  list.data = data.map(function(v) {
+    return new freebase.Topic(v)
+  })
+  list.get = function(property, callback, options) {
+    var doit = function(t, cb) {
+      t.get(property, cb, options)
+    }
+    slow(list.data, doit, {}, function(r) {
+      list.data = list.data.map(function(t, i) {
+        t[property] = r[i]
+        return t
+      })
+      callback(list.data)
+    })
+  }
+  list.set = function() {}
+  list.select = function() {}
+  list.reject = function() {}
+  list.each = function(method, done){
+    var doit=function(t,cb){
+      cb=cb||console.log
+      method(t,cb)
+    }
+    done=done||freebase.default_callback
+    slow(list.data, doit, done)
+  }
 }
 
 
-  freebase.Option = function(data) {
-    var option = this;
-    data = data || {}
-    option.key = freebase.api_key
-    option.property = data.property //|| function(){new Property(data.property);}
-    option.type = data.type //|| function(){return new freebase.Property(data.property).type;}
-    option=fns.extend(option, data)
+freebase.Option = function(data) {
+  var option = this;
+  data = data || {}
+  option.key = freebase.api_key
+  option.property = data.property //|| function(){new Property(data.property);}
+  option.type = data.type //|| function(){return new freebase.Property(data.property).type;}
+  option = fns.extend(option, data)
+}
+
+
+
+freebase.mql_encode = function(s) {
+  this.doc = "quote a unicode string to turn it into a valid mql /type/key/value"
+  if (!s) {
+    return ''
   }
-
-
-
-  freebase.mql_encode = function(s) {
-    this.doc = "quote a unicode string to turn it into a valid mql /type/key/value"
-    if (!s) {
-      return ''
-    }
-    s = s.replace(/  /, ' ');
-    s = s.replace(/^\s+|\s+$/, '');
-    s = s.replace(/ /g, '_');
-    var mqlkey_start = 'A-Za-z0-9';
-    var mqlkey_char = 'A-Za-z0-9_-';
-    var MQLKEY_VALID = new RegExp('^[' + mqlkey_start + '][' + mqlkey_char + ']*$');
-    var MQLKEY_CHAR_MUSTQUOTE = new RegExp('([^' + mqlkey_char + '])', 'g');
-    if (MQLKEY_VALID.exec(s)) // fastpath
-      return s;
-    var convert = function(a, b) {
-      var hex = b.charCodeAt(0).toString(16).toUpperCase();
-      if (hex.length == 2) hex = '00' + hex;
-      if (hex.length == 3) hex = '0' + hex;
-      return '$' + hex;
-    };
-    var x = s.replace(MQLKEY_CHAR_MUSTQUOTE, convert);
-    if (x.charAt(0) == '-' || x.charAt(0) == '_') {
-      x = convert(x, x.charAt(0)) + x.substr(1);
-    }
-    return x;
+  s = s.replace(/  /, ' ');
+  s = s.replace(/^\s+|\s+$/, '');
+  s = s.replace(/ /g, '_');
+  var mqlkey_start = 'A-Za-z0-9';
+  var mqlkey_char = 'A-Za-z0-9_-';
+  var MQLKEY_VALID = new RegExp('^[' + mqlkey_start + '][' + mqlkey_char + ']*$');
+  var MQLKEY_CHAR_MUSTQUOTE = new RegExp('([^' + mqlkey_char + '])', 'g');
+  if (MQLKEY_VALID.exec(s)) // fastpath
+    return s;
+  var convert = function(a, b) {
+    var hex = b.charCodeAt(0).toString(16).toUpperCase();
+    if (hex.length == 2) hex = '00' + hex;
+    if (hex.length == 3) hex = '0' + hex;
+    return '$' + hex;
+  };
+  var x = s.replace(MQLKEY_CHAR_MUSTQUOTE, convert);
+  if (x.charAt(0) == '-' || x.charAt(0) == '_') {
+    x = convert(x, x.charAt(0)) + x.substr(1);
   }
+  return x;
+}
 
 
-  freebase.fetch=function(topic, callback, options){
-    this.description="lookup a specific property in the topic api"
-    callback=callback||freebase.default_callback;
-    options = new freebase.Option(options);
-    freebase.topic_api(topic, function(result){
-      return callback(result.property)
-    }, options)
-  }
+freebase.fetch = function(topic, callback, options) {
+  this.description = "lookup a specific property in the topic api"
+  callback = callback || freebase.default_callback;
+  options = new freebase.Option(options);
+  freebase.topic_api(topic, function(result) {
+    return callback(result.property)
+  }, options)
+}
 
 
-  freebase.topic_api=function(topic, callback, options) {
-    this.description="call the freebase topic api"
-    options = new freebase.Option(options);
-    callback=callback||freebase.default_callback;
-    var filter = options.property || options.type || "all";
-    var url = freebase.host + '/topic' + topic.mid + '?key=' + (options.key || '') + '&filter=' + (filter || "all")
-    fns.http(url, function(result) {
-      return callback(result)
-    }, options)
-  }
+freebase.topic_api = function(topic, callback, options) {
+  this.description = "call the freebase topic api"
+  options = new freebase.Option(options);
+  callback = callback || freebase.default_callback;
+  var filter = options.property || options.type || "all";
+  var url = freebase.host + '/topic' + topic.mid + '?key=' + (options.key || '') + '&filter=' + (filter || "all")
+  fns.http(url, function(result) {
+    return callback(result)
+  }, options)
+}
 
-  freebase.search_api=function(str, callback, options) {
-    this.description="call the freebase search api"
-    options = new freebase.Option(options);
-    callback=callback||freebase.default_callback;
-    var url = freebase.host + '/search?query=' + encodeURIComponent(str) + '&key=' + (options.key || '')
-    fns.http(url, function(r) {
-      var list=new freebase.Topiclist(r.result)
-      return callback(list)
-    })
-  }
+freebase.search_api = function(str, callback, options) {
+  this.description = "call the freebase search api"
+  options = new freebase.Option(options);
+  callback = callback || freebase.default_callback;
+  var url = freebase.host + '/search?query=' + encodeURIComponent(str) + '&key=' + (options.key || '')
+  fns.http(url, function(r) {
+    var list = new freebase.Topiclist(r.result)
+    return callback(list)
+  })
+}
 
-  freebase.reconcile_property=function(str, callback, options){
-      callback=callback||freebase.default_callback;
-      options = new freebase.Option(options)
-      if(fns.isid(str)){
-        callback(new freebase.Property(str))
+freebase.mqlread = function(query, callback, options) {
+  this.description = "call the freebase mql api"
+  options = new freebase.Option(options);
+  callback = callback || freebase.default_callback;
+  var url = freebase.host + '/mqlread?query=' + encodeURIComponent(JSON.stringify(query)) + '&key=' + (options.key || '') + "&cursor=" + (options.cursor || "")
+  fns.http(url, function(r) {
+    return callback(r)
+  })
+}
+
+freebase.mqlwrite = function(query, callback, options) {
+  this.description = "call the freebase mql api"
+  options = new freebase.Option(options);
+  callback = callback || freebase.default_callback;
+  options.oauth_token = options.oauth_token || options.oauth || options.token
+  var url = freebase.host + '/mqlwrite?query=' + encodeURIComponent(JSON.stringify(query)) + "&oauth_token=" + (options.oauth_token || "")
+  fns.http(url, callback)
+}
+
+freebase.paginate = function(query, callback, options) {
+  this.description = "call the freebase mql api until it finishes, or reaches your maximum"
+  options = new freebase.Option(options);
+  callback = callback || freebase.default_callback;
+  options.max = options.max || options.maximum || 6000;
+  var all = [];
+  (function iterate(cursor) {
+    options.cursor = cursor
+    console.log(all.length)
+    freebase.mqlread(query, function(r) {
+      all = all.concat(r.result)
+      if (r.cursor && all.length < options.max) {
+        iterate(r.cursor)
+      } else {
+        callback(all)
       }
-      else if(freebase.data.properties[str]){
-        callback(new freebase.Property(freebase.data.properties[str]))
-      }
-    }
+    }, options)
+  })()
+}
 
-  // export for AMD / RequireJS
-  if (typeof define !== 'undefined' && define.amd) {
-    define([], function() {
-      return freebase;
-    });
+freebase.reconcile_property = function(str, callback, options) {
+  callback = callback || freebase.default_callback;
+  options = new freebase.Option(options)
+  if (fns.isid(str)) {
+    callback(new freebase.Property(str))
+  } else if (freebase.data.properties[str]) {
+    callback(new freebase.Property(freebase.data.properties[str]))
   }
-  // export for Node.js
-  else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = freebase;
-  }
+}
 
-  return freebase;
+// export for AMD / RequireJS
+if (typeof define !== 'undefined' && define.amd) {
+  define([], function() {
+    return freebase;
+  });
+}
+// export for Node.js
+else if (typeof module !== 'undefined' && module.exports) {
+  module.exports = freebase;
+}
+
+return freebase;
 
 })()
 
@@ -241,3 +312,34 @@ function render_results(results){
 //     console.log(JSON.stringify(names, null, 2));
 //    })
 // })
+// query=[
+//     {
+//       "id": "/en/radiohead",
+//       "type": [
+//         {
+//           "connect": "insert",
+//           "id": "/music/artist"
+//         }
+//       ]
+//     }
+//   ]
+// var dirty=require('/Users/spencer/mountain/dirty');
+//  options={oauth:"ya29.AHES6ZQUWg_VCnz4YfPnojkGK6ZWcfFzba-lI9x1UvivkRfm"}
+// // freebase.mqlwrite(query, console.log, options)
+
+// // radiohead = new freebase.Topic("/en/radiohead")
+// // radiohead.add_type("/music/artist",console.log,options)
+// done=function(r){
+//   console.log(JSON.stringify(r, null, 2));
+// }
+// end=function(r){
+//   console.log("==done==")
+//   console.log(JSON.stringify(r, null, 2));
+// }
+
+// list = new freebase.Topiclist(["/en/thom_yorke","/en/paris_hilton"])
+// list.each(function(t,cb){
+//   //t.get("name",cb)
+//   t.add_type("/people/person",cb, options)
+// }, end)
+//list.each()
